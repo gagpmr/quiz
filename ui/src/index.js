66341f3365903138200aaeeeb84588ee15ff3a20
gagpmr <absolutely2great@gmail.com>
2019-04-17 09:22:19 +0530

    ui index.js

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import gql from "graphql-tag";
+import React from "react";
+import { ApolloProvider, Query } from "react-apollo";
+import ReactDOM from "react-dom";
+import Pages from "./pages";
+import Login from "./pages/login";
+import { typeDefs } from "./resolvers";
+import injectStyles from "./styles";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: "http://localhost:4000/graphql",
+    headers: {
+      authorization: localStorage.getItem("token")
+    }
+  }),
+  typeDefs
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem("token")
+  }
+});
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+injectStyles();
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Query query={IS_LOGGED_IN}>
+      {({ data }) => (data.isLoggedIn ? <Pages /> : <Login />)}
+    </Query>
+  </ApolloProvider>,
+  document.getElementById("root")
+);
