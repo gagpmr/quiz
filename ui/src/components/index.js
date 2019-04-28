c43f5daeee6f2efc410b526b0dfb89059e1077aa
Gaganpreet Singh Parmar <gagpmr@localhost.localdomain>
2019-04-28 12:24:13 +0530

    reset

--- ui/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Header } from "./header";
-export { default as LaunchTile } from "./launch-tile";
-export { default as Loading } from "./loading";
-export { default as LoginForm } from "./login-form";
-export { default as PageContainer } from "./page-container";
--- ui/src/pages/profile.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Profile() {
-  return <div id="Profile">My Profile</div>;
-}
